a04c9a111169f8fad1e275eb3d1175976909588f
Daniel <cermak222@gmail.com>
2019-10-13 10:17:08 -0500

    Updates

---
+++ precache-manifest.bae138e76fee4cc0a1ab95d15dca57f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f1fd6d1113580c7123bd5f9da3c703",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "887420f2f74160299c71",
+    "url": "/project-4-assessment/static/css/main.5dfaf74f.chunk.css"
+  },
+  {
+    "revision": "adcc6c9126018b755d0a",
+    "url": "/project-4-assessment/static/js/2.25804772.chunk.js"
+  },
+  {
+    "revision": "887420f2f74160299c71",
+    "url": "/project-4-assessment/static/js/main.657a0ed1.chunk.js"
+  },
+  {
+    "revision": "75de8ec33a8a78802050",
+    "url": "/project-4-assessment/static/js/runtime-main.71652b97.js"
+  }
+]);
